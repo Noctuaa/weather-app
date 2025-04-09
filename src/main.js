bf0e636a3f14cff1bce701fb34f75b58c047fe24
Noctua <titgeogeo@gmail.com>
2025-04-09 16:31:03 +0200

    chore: add Font Awesome dependency

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import './assets/styles/main.css';
 import App from './App.vue';
 import router from './router';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add();
+
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.mount('#app');
